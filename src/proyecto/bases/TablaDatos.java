e24bff29c6022121eee79b9637148627ff8cf57d
b45818 <b45818@ECCI-LBW-105-01.ecci.ucr.ac.cr>
2017-12-09 16:44:45 -0600

    no estoy seguro de que cambio

--- src/proyecto/bases/TablaDatos.java
+++ src/proyecto/bases/TablaDatos.java
@@ -12,7 +12,6 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.Vector;
-import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /**
