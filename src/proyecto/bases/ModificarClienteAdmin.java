61f77973e0e3670c9dadb3f501b5e97179bdb50f
Fernando <Fernando@Fernando-PC>
2017-12-10 04:41:37 -0600

    listo admin cliente

--- src/proyecto/bases/ModificarClienteAdmin.java
+++ src/proyecto/bases/ModificarClienteAdmin.java
@@ -5,17 +5,35 @@
  */
 package proyecto.bases;
 
+import java.sql.*;
+import javax.swing.*;
+import java.util.Collections;
+
 /**
  *
  * @author b22539
  */
 public class ModificarClienteAdmin extends javax.swing.JFrame {
+    private String conexion;
+    private boolean type;
+    final Runnable function;
+    private String llaveId;
+    private String llaveCorreo;
 
     /**
      * Creates new form ModificarCliente
      */
-    public ModificarClienteAdmin() {
+    public ModificarClienteAdmin(String conexion, boolean type, Runnable function, Object[][] nuevos) {
+        this.function = function;
+        this.conexion = conexion;
+        this.type = type;
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         initComponents();
+        if(!type){
+            llaveId = nuevos[0][0].toString();
+            id.setText(nuevos[0][0].toString());
+            nombre.setText(nuevos[0][2].toString());
+        }
     }
 
     /**
@@ -27,29 +45,29 @@ public class ModificarClienteAdmin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTextField2 = new javax.swing.JTextField();
-        jButton2 = new javax.swing.JButton();
-        jTextField3 = new javax.swing.JTextField();
+        id = new javax.swing.JTextField();
+        aceptar = new javax.swing.JButton();
+        correo = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        jTextField7 = new javax.swing.JTextField();
+        tipo = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
-        jTextField5 = new javax.swing.JTextField();
+        telefono = new javax.swing.JTextField();
+        empleado = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
-        jTextField6 = new javax.swing.JTextField();
+        funcion = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton3 = new javax.swing.JButton();
+        nombre = new javax.swing.JTextField();
+        regresar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton2.setText("Aceptar");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        aceptar.setText("Aceptar");
+        aceptar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                aceptarActionPerformed(evt);
             }
         });
 
@@ -59,19 +77,7 @@ public class ModificarClienteAdmin extends javax.swing.JFrame {
 
         jLabel5.setText("Empleado:");
 
-        jTextField7.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField7ActionPerformed(evt);
-            }
-        });
-
         jLabel6.setText("Función:");
-
-        jTextField5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField5ActionPerformed(evt);
-            }
-        });
 
         jLabel1.setText("Nombre:");
 
@@ -79,10 +85,10 @@ public class ModificarClienteAdmin extends javax.swing.JFrame {
 
         jLabel3.setText("Correo:");
 
-        jButton3.setText("Regresar");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        regresar.setText("Regresar");
+        regresar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                regresarActionPerformed(evt);
             }
         });
 
@@ -94,9 +100,9 @@ public class ModificarClienteAdmin extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton3)
+                        .addComponent(regresar)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton2))
+                        .addComponent(aceptar))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING)
@@ -109,13 +115,13 @@ public class ModificarClienteAdmin extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(jTextField1)
-                                .addComponent(jTextField2)
-                                .addComponent(jTextField3)
-                                .addComponent(jTextField4)
-                                .addComponent(jTextField5)
-                                .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, 202, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jTextField7))))
+                                .addComponent(nombre)
+                                .addComponent(id)
+                                .addComponent(correo)
+                                .addComponent(telefono)
+                                .addComponent(empleado)
+                                .addComponent(funcion, javax.swing.GroupLayout.PREFERRED_SIZE, 202, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(tipo))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -124,61 +130,116 @@ public class ModificarClienteAdmin extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel7)
-                    .addComponent(jTextField7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(tipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(nombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(id, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(correo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(15, 15, 15)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(telefono, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
-                    .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(empleado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel6)
-                    .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(funcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3))
+                    .addComponent(aceptar)
+                    .addComponent(regresar))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField7ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField7ActionPerformed
+    private void aceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_aceptarActionPerformed
+        if(type){
+            Object[] atributos = new Object[2];
+            atributos[0] = id.getText();
+            atributos[1] = nombre.getText();
+            String query = "INSERT INTO DBO.CLIENTE VALUES(?,?)";
+            TablaDatos.executeUpdate(conexion, query, atributos);
+            if(tipo.getText().equals("particular")){
+                atributos = new Object[3];
+                atributos[0] = id.getText();
+                atributos[1] = correo.getText();
+                atributos[2] = telefono.getText();
+                query = "INSERT INTO DBO.PARTICULAR VALUES(?,?,?)";
+                TablaDatos.executeUpdate(conexion, query, atributos);
+            }else if(tipo.getText().equals("empresa")){
+                atributos = new Object[5];
+                atributos[0] = id.getText();
+                atributos[1] = correo.getText();
+                atributos[2] = empleado.getText();
+                atributos[3] = funcion.getText();
+                atributos[4] = telefono.getText();
+                query = "INSERT INTO DBO.EMPLEADO VALUES(?,?,?,?,?)";
+                TablaDatos.executeUpdate(conexion, query, atributos);
+            }
+        }else{
+            Object[] atributos = new Object[3];
+            atributos[0] = id.getText();
+            atributos[1] = nombre.getText();
+            atributos[2] = llaveId;
+            String query = "UPDATE DBO.CLIENTE SET IDCLIENTE = ?, NOMBRECLIENTE = ? WHERE IDCLIENTE = ?";
+            TablaDatos.executeUpdate(conexion, query, atributos);
+            if(tipo.getText().equals("particular")){
+                atributos = new Object[4];
+                atributos[0] = id.getText();
+                atributos[1] = correo.getText();
+                atributos[2] = telefono.getText();
+                atributos[3] = llaveId;
+                query = "UPDATE DBO.PARTICULAR SET IDCLIENTE = ?, CORREOPARTICULAR = ?, TELEFONOPARTICULAR = ? WHERE IDCLIENTE = ?";
+                TablaDatos.executeUpdate(conexion, query, atributos);
+            }else if(tipo.getText().equals("empresa")){
+                atributos = new Object[7];
+                atributos[0] = id.getText();
+                atributos[1] = correo.getText();
+                atributos[2] = empleado.getText();
+                atributos[3] = funcion.getText();
+                atributos[4] = telefono.getText();
+                atributos[5] = llaveId;
+                atributos[6] = llaveCorreo;
+                query = "UPDATE DBO.EMPLEADO SET IDCLIENTE = ?, CORREOEMPLEADO = ?, NOMBREEMPLEADO = ?,"
+                        + "FUNCION = ?, TELEFONOEMPLEADO = ? WHERE IDCLIENTE = ? AND CORREOEMPLEADO = ?";
+                TablaDatos.executeUpdate(conexion, query, atributos);
+            }
+        }
+        actualizar();
+    }//GEN-LAST:event_aceptarActionPerformed
 
-    private void jTextField5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField5ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField5ActionPerformed
+    private void regresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_regresarActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_regresarActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
-
+    private void actualizar() {
+        try {
+            function.run();
+        } catch (Exception ex) {
+            System.err.print("Error al retornar al padre");
+        }
+        this.dispose();
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JButton aceptar;
+    private javax.swing.JTextField correo;
+    private javax.swing.JTextField empleado;
+    private javax.swing.JTextField funcion;
+    private javax.swing.JTextField id;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -186,12 +247,9 @@ public class ModificarClienteAdmin extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
-    private javax.swing.JTextField jTextField7;
+    private javax.swing.JTextField nombre;
+    private javax.swing.JButton regresar;
+    private javax.swing.JTextField telefono;
+    private javax.swing.JTextField tipo;
     // End of variables declaration//GEN-END:variables
 }
